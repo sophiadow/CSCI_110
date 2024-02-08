adb173f0438a07c7b5d5cbc55cb4118e66914e98
sophiadow <158004118+sophiadow@users.noreply.github.com>
2024-02-07 18:07:30 -0700

    Add files via upload

---
+++ Week3.py
@@ -0,0 +1,13 @@
+#Problem 1
+All = "All"
+Work = "work"
+an = "and"
+no = "no"
+play = "play"
+makes = "makes"
+Jack = "Jack"
+A = "A"
+dull = "dull"
+boy = "boy"
+
+print(All, Work, an, no, play, makes, Jack, A, dull, boy)
