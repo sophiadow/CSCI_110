0ac386d8e5c91f3e7e7c0507359f78c268a01267
sophiadow <158004118+sophiadow@users.noreply.github.com>
2024-02-16 19:24:05 -0700

    Add files via upload

---
+++ week4.py
@@ -0,0 +1,19 @@
+import turtle
+wn = turtle.Screen()
+wn.bgcolor("pink")
+daisy = turtle.Turtle()
+daisy.color("purple")
+
+daisy.forward(300)
+daisy.right(144)
+daisy.forward(300)
+daisy.right(144)
+daisy.forward(300)
+daisy.right(144)
+daisy.forward(300)
+daisy.right(144)
+daisy.forward(300)
+daisy.right(144)
+wn.mainloop()
+
+ 
