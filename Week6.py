93c3894c732f08ba357d12ca888f6012799dff22
sophiadow <158004118+sophiadow@users.noreply.github.com>
2024-03-03 17:04:23 -0700

    Add files via upload

---
+++ Week6.py
@@ -0,0 +1,33 @@
+# Week 5 HW
+
+# Section 5.14
+
+# Problem 1
+def print_day_of_week(x):
+    if x == 0:
+        print("Sunday")
+    elif x == 1:
+        print("Monday")
+    elif x == 2:
+        print("Tuesday")
+    elif x == 3:
+        print("Wednesday")
+    elif x == 4:
+        print("Thursday")
+    elif x == 5:
+        print("Friday")
+    else:
+        print("Not a weekday")
+print_day_of_week(5)
+
+
+
+
+    
+
+
+
+
+
+
+
